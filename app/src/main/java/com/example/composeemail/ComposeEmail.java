fc5c405e39dbf1b2a0bba41605dd20bb518793d2
Josh <joshuadronyi@gmail.com>
2019-09-12 14:30:58 -0400

    Initial commit: Forgot to commit at the beginning. Tapped out most things before remembering to commit.

---
+++ app/src/main/java/com/example/composeemail/ComposeEmail.java
@@ -0,0 +1,14 @@
+package com.example.composeemail;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ComposeEmail extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_compose_email);
+    }
+}
